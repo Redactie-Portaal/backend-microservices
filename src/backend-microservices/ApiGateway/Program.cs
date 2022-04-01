ee6d32b9d97f6cec9978c1d0e6a3e18df1bb20c1
I412999 <I412999@fhict.nl>
2022-04-01 10:32:45 +0200

    Fixed ocelot files

--- src/backend-microservices/ApiGateway/Program.cs
+++ src/backend-microservices/ApiGateway/Program.cs
@@ -16,7 +16,7 @@ namespace ApiGateway
                     .SetBasePath(hostingContext.HostingEnvironment.ContentRootPath)
                     .AddJsonFile("appsettings.json", true, true)
                     .AddJsonFile($"appsettings.{hostingContext.HostingEnvironment.EnvironmentName}.json", true, true)
-                    .AddJsonFile("ocelot.json")
+                    .AddJsonFile($"ocelot.{hostingContext.HostingEnvironment.EnvironmentName}.json", false, true)
                     .AddEnvironmentVariables();
             })
             .ConfigureServices(s => {
