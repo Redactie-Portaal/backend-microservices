dfd3d5b7b76f525dce40979c0bf1d2d5bc79f34b
I412999 <I412999@fhict.nl>
2022-04-01 11:53:00 +0200

    Added test unit test

---
+++ src/services/UserService.Tests/UnitTests/UserTests.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UserService.Entities;
+using Xunit;
+
+namespace UserService.Tests.UnitTests
+{
+    public class UserTests: IDisposable
+    {
+
+        public UserTests()
+        {
+            
+        }
+
+        [Fact]
+        public async Task TestUsers()
+        {
+            var users = new List<User>
+            {
+                new User { Id = 1, }
+            };
+
+            Assert.Single(users);
+        }
+
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
