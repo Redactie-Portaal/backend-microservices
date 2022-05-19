bbc4be089c5a5bb48281a3ad55dd35b6ddf868f7
Sina Nazar Soroush <13730935+MrTech12@users.noreply.github.com>
2022-05-19 14:05:56 +0200

    Add: entities for NewsItem
    * Added entities for NewsItem and all tables that have a relation
    with it.

---
+++ src/services/NewsItemService/Data/NewsItemServiceDatabaseContext.cs
@@ -0,0 +1,70 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NewsItemService.Entities;
+
+namespace NewsItemService.Data
+{
+    public class NewsItemServiceDatabaseContext : DbContext
+    {
+        /// <summary>
+        /// Constructor of the NewsItemServiceDatabaseContext class
+        /// </summary>
+        public NewsItemServiceDatabaseContext()
+        {
+        }
+
+        /// <summary>
+        /// Constructor of the NewsItemServiceDatabaseContext class with options, used for Unittesting
+        /// Database options can be given, to switch between local and remote database
+        /// </summary>
+        /// <param name="options">Database options</param>
+        public NewsItemServiceDatabaseContext(DbContextOptions<NewsItemServiceDatabaseContext> options) : base(options)
+        {
+        }
+
+        /// <summary>
+        /// DbSet for the NewsItem class, A DbSet represents the collection of all entities in the context. 
+        /// DbSet objects are created from a DbContext using the DbContext.Set method.
+        /// </summary>
+        public DbSet<NewsItem> NewsItems { get; set; }
+        /// <summary>
+        /// DbSet for the Author class, A DbSet represents the collection of all entities in the context. 
+        /// DbSet objects are created from a DbContext using the DbContext.Set method.
+        /// </summary>
+        public DbSet<Author> Authors { get; set; }
+        /// <summary>
+        /// DbSet for the Category class, A DbSet represents the collection of all entities in the context. 
+        /// DbSet objects are created from a DbContext using the DbContext.Set method.
+        /// </summary>
+        public DbSet<Category> Categories { get; set; }
+        /// <summary>
+        /// DbSet for the Tag class, A DbSet represents the collection of all entities in the context. 
+        /// DbSet objects are created from a DbContext using the DbContext.Set method.
+        /// </summary>
+        public DbSet<Tag> Tags { get; set; }
+        /// <summary>
+        /// DbSet for the Publication class, A DbSet represents the collection of all entities in the context. 
+        /// DbSet objects are created from a DbContext using the DbContext.Set method.
+        /// </summary>
+        public DbSet<Publication> Publications { get; set; }
+
+        /// <summary>
+        /// OnConfiguring builds the connection between the database and the API using the given connection string
+        /// </summary>
+        /// <param name="optionsBuilder">Used for adding options to the database to configure the connection.</param>
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                var dbString = Environment.GetEnvironmentVariable("redactieportaal_db_string");
+                if (string.IsNullOrWhiteSpace(dbString))
+                {
+                    throw new MissingFieldException("Database environment variable not found.");
+                }
+
+                optionsBuilder.UseNpgsql(Environment.GetEnvironmentVariable("redactieportaal_db_string").Replace("DATABASE_NAME", "newsitemservice"));
+            }
+
+            base.OnConfiguring(optionsBuilder);
+        }
+    }
+}
---
+++ src/services/NewsItemService/Entities/Author.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace NewsItemService.Entities
+{
+    public class Author
+    {
+        [Key]
+        [Required]
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public ICollection<NewsItem> NewsItems { get; set; }
+    }
+}
