fa6a1f579065b49b62cb35a25b53a0c19940148c
Sina Nazar Soroush <13730935+MrTech12@users.noreply.github.com>
2022-06-02 13:25:20 +0200

    Edit: fixed running all integration tests
    * Placed connection string inside testfile.
    * Added Bash Script + GitHub Actions to run the integration tests in
    a CI environment.

--- tests/NewsItemService.Tests/IntegrationTests/NewsItemRepositoryIntegrationTest.cs
+++ tests/NewsItemService.Tests/IntegrationTests/NewsItemRepositoryIntegrationTest.cs
@@ -13,29 +13,25 @@ using Xunit;
 
 namespace NewsItemService.Tests.IntegrationTests
 {
-    public class NewsItemRepositoryIntegrationTest: IDisposable, IClassFixture<TestConfiguration>
+    public class NewsItemRepositoryIntegrationTest: IDisposable
     {
         private readonly NewsItemServiceDatabaseContext _databaseContext;
         private readonly NewsItemRepository _newsItemRepository;
-        private readonly TestConfiguration _testConfiguration;
-        private readonly IConfiguration _configuration;
 
-        public NewsItemRepositoryIntegrationTest(TestConfiguration testConfiguration)
+        public NewsItemRepositoryIntegrationTest()
         {
-            this._testConfiguration = testConfiguration;
-            this._configuration = _testConfiguration.GetTestDataConfiguration();
-            string connectionString = this._configuration.GetConnectionString("LinkContext");
-
+            string connectionString = "Server=localhost;Port=1111;Database=integrationtests;UserId=developer;Password=developer";
             var serviceProvider = new ServiceCollection().AddEntityFrameworkNpgsql().BuildServiceProvider();
 
             var builder = new DbContextOptionsBuilder<NewsItemServiceDatabaseContext>();
             builder.UseNpgsql(connectionString).UseInternalServiceProvider(serviceProvider);
             this._databaseContext = new NewsItemServiceDatabaseContext(builder.Options);
 
-            this._databaseContext.Database.Migrate();
+            this._databaseContext.Database.EnsureCreated();
+
+            SeedData(this._databaseContext);
 
             this._newsItemRepository = new NewsItemRepository(this._databaseContext);
-            SeedData(this._databaseContext);
         }
 
         /*
@@ -53,13 +49,14 @@ namespace NewsItemService.Tests.IntegrationTests
         {
             var authors = new List<Author>()
             {
-                new Author(){ Id = 1, Name = "TestAuthor", NewsItems = null}
+                new Author(){ Id = 1, Name = "TestAuthor", NewsItems = null},
+                new Author(){ Id = 2, Name = "TestAuthor2", NewsItems = null}
             };
 
             var newsItems = new List<NewsItem>()
             {
-                new NewsItem { Id = 1, Authors = authors, Created = DateTime.Now, Updated = DateTime.Now, Status = NewsItemStatus.Publication},
-                new NewsItem { Id = 3, Authors = authors, Created = DateTime.Now, Updated = DateTime.Now, Status = NewsItemStatus.Publication}
+                new NewsItem { Id = 1, Authors = authors, Created = DateTime.Now.ToUniversalTime(), Updated = DateTime.Now.ToUniversalTime(), Status = NewsItemStatus.Publication},
+                new NewsItem { Id = 2, Authors = authors, Created = DateTime.Now.ToUniversalTime(), Updated = DateTime.Now.ToUniversalTime(), Status = NewsItemStatus.Publication}
             };
 
             if (!context.NewsItems.Any())
@@ -82,7 +79,7 @@ namespace NewsItemService.Tests.IntegrationTests
         [Fact]
         public void GetSingleItemNonexistent()
         {
-            var result = _newsItemRepository.Get(2);
+            var result = _newsItemRepository.Get(5);
             Assert.Null(result);
         }
 
@@ -119,21 +116,21 @@ namespace NewsItemService.Tests.IntegrationTests
         [Fact]
         public void GetAfterDate()
         {
-            var result = _newsItemRepository.GetAfter(DateTime.Now.AddDays(1), 1, 2);
+            var result = _newsItemRepository.GetAfter(DateTime.Now.AddDays(1).ToUniversalTime(), 1, 2);
             Assert.Empty(result);
         }
 
         [Fact]
         public void GetBeforeDate()
         {
-            var result = _newsItemRepository.GetBefore(DateTime.Now.AddDays(-1), 1, 2);
+            var result = _newsItemRepository.GetBefore(DateTime.Now.AddDays(-1).ToUniversalTime(), 1, 2);
             Assert.Empty(result);
         }
 
         [Fact]
         public void GetBetweenDate()
         {
-            var result = _newsItemRepository.GetBetween(DateTime.Now.AddDays(-1), DateTime.Now.AddDays(1), 1, 2);
+            var result = _newsItemRepository.GetBetween(DateTime.Now.AddDays(-1).ToUniversalTime(), DateTime.Now.AddDays(1).ToUniversalTime(), 1, 2);
             Assert.Equal(2, result.Count);
         }
         #endregion
@@ -141,8 +138,14 @@ namespace NewsItemService.Tests.IntegrationTests
         [Fact]
         public void AddNewNewsItem()
         {
-            Author author = new Author() { Name = "TestAuthor2", NewsItems = null };
-            _newsItemRepository.Post(new NewsItem { Authors = new List<Author>(){ author }, Created = DateTime.Now, Updated = DateTime.Now, Status = NewsItemStatus.Production });
+            
+            var newsItem = new NewsItem() { Id = 3, Created = DateTime.Now.ToUniversalTime(), Updated = DateTime.Now.ToUniversalTime(), Status = NewsItemStatus.Production };
+
+            var author = _databaseContext.Authors.Include("NewsItems").FirstOrDefault(a => a.Id == 1);
+            newsItem.Authors = new List<Author>();
+            newsItem.Authors.Add(author);
+
+            _newsItemRepository.Post(newsItem);
             var result = _newsItemRepository.Get(1, 3);
             Assert.Equal(3, result.Count);
         }
---
+++ tests/NewsItemService.Tests/IntegrationTests/run-integration-tests.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+# Explanation
+# Integration tests make use of a Docker image for running the database.
+# In order to run this database in a CI environment efficiently, this script has been created.
+# The script executes the Docker compose file, which run the database.
+# It then looks in the docker logs, to see if the database is ready for the tests.
+# After the database is ready, the integration tests will run.
+
+# Script workflow
+# 1. Spin up a PostgreSQL Docker environmnet
+# 2. Wait for PostgreSQL to boot up
+# 3. Run the Integration tests
+
+printf "Message: Helper script for running Integration tests. \n"
+
+printf "Message: current working directory: "
+echo $PWD
+printf '\n'
+
+printf "Message: Running Docker compose file to pull up & boot up PostgreSQL container. \n"
+printf "Message: Command that is executed: 'docker-compose up -d' \n"
+docker-compose -f docker-compose.integrationtests.yml up -d
+printf "\n"
+
+printf "Message: Waiting for PostgreSQL to boot up, in a 5 second interval \n"
+printf "Message: Command that is executed: 'docker logs postgresql' \n"
+until [[ $(docker logs postgresql) == *"database system is ready to accept connections"* ]]
+do
+  printf "Message: still waiting on PostgreSQL to boot..."
+  sleep 5s
+done
+printf "\n"
+
+printf "Message: The environment is ready. Time to run the integration tests. \n"
+printf "Message: Command that executed: {dotnet test --logger 'trx;LogFileName=test-results.trx' --collect:'XPlat Code Coverage'} \n"
+dotnet test $1 --logger "trx;LogFileName=test-results.trx" --collect:"XPlat Code Coverage"
+printf "\n"
