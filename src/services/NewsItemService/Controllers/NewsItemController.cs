91000aa4f15df7b0eda753915e21105abd47cacf
I412999 <I412999@fhict.nl>
2022-06-15 22:43:38 +0200

    Added routing

--- src/services/NewsItemService/Controllers/NewsItemController.cs
+++ src/services/NewsItemService/Controllers/NewsItemController.cs
@@ -11,7 +11,7 @@ using RabbitMQLibrary;
 
 namespace NewsArticleService.Controllers
 {
-    [Route("api/[controller]")]
+    [Route("[controller]")]
     [ApiController]
     public class NewsItemController : ControllerBase
     {
