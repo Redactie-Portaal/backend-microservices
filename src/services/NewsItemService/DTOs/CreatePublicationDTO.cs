9e1e196b9817869588993f9a7e480d1ca85cb18b
Lex Krooswijk <lexkrooswijk@live.nl>
2022-06-21 11:40:00 +0200

    + create publication

---
+++ src/services/NewsItemService/DTOs/CreatePublicationDTO.cs
@@ -0,0 +1,9 @@
+namespace NewsItemService.DTOs
+{
+    public class CreatePublicationDTO
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string? Icon { get; set; }
+    }
+}
